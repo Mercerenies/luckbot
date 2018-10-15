b6cd5c418faa4248d81a207f524bd63ce82cafba
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2018-10-15 15:53:10 -0400

    Text

--- grid.py
+++ grid.py
@@ -1,5 +1,5 @@
 
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import random
 
 import alakazam as zz
@@ -45,11 +45,25 @@ class GridConfig:
     WIDTH = 64
     HEIGHT = 64
 
-    def __init__(self, rows, cols, bgcolor='white', fgcolor='black'):
+    def __init__(self, rows, cols, text='', bgcolor='white', fgcolor='black'):
         self.rows = rows
         self.cols = cols
+        self.text = CallGuard(text)
         self.bgcolor = CallGuard(bgcolor)
         self.fgcolor = CallGuard(fgcolor)
+
+    @staticmethod
+    def print_text(draw, text, x, y):
+        WIDTH, HEIGHT = GridConfig.WIDTH, GridConfig.HEIGHT
+
+        font_size = 12
+        while True:
+            font = ImageFont.truetype('LiberationSans-Regular.ttf', font_size)
+            w, h = font.getsize(text)
+            if w < WIDTH and h < HEIGHT:
+                break
+            font_size -= 1
+        draw.text((x - w / 2, y - h / 2), text, font=font, fill='black')
 
     def make_grid(self):
         WIDTH, HEIGHT = GridConfig.WIDTH, GridConfig.HEIGHT
@@ -61,4 +75,6 @@ class GridConfig:
             pos0 = (i * WIDTH, j * HEIGHT)
             pos1 = ((i + 1) * WIDTH, (j + 1) * HEIGHT)
             draw.rectangle([pos0, pos1], fill=self.bgcolor(i, j), outline=self.fgcolor(i, j))
+            text = self.text(i, j)
+            GridConfig.print_text(draw, text, (i + 0.5) * WIDTH, (j + 0.5) * HEIGHT)
         return image
