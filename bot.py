62d168014c0ea382827fa0607b829e0cb7ff7b9d
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2018-10-15 16:15:07 -0400

    Sending image of grid factored out

--- bot.py
+++ bot.py
@@ -586,6 +586,14 @@ def role(ctx, cmd, *args):
     elif cmd == "remove":
         yield from role_remove(ctx, *args)
 
+@asyncio.coroutine
+def send_image_of_grid(cfg, channel, filename="image.png"):
+    image = cfg.make_grid()
+    with BytesIO() as buffer:
+        image.save(buffer, 'PNG')
+        buffer.seek(0)
+        yield from bot.send_file(channel, buffer, filename=filename)
+
 @bot.command(pass_context=True)
 @asyncio.coroutine
 def grid(ctx, dims="3x3"):
@@ -596,11 +604,7 @@ def grid(ctx, dims="3x3"):
     """
     n, m = map(int, re.findall(r"[0-9]+", dims))
     cfg = GridConfig(rows=n, cols=m)
-    image = cfg.make_grid()
-    with BytesIO() as buffer:
-        image.save(buffer, 'PNG')
-        buffer.seek(0)
-        yield from bot.send_file(ctx.message.channel, buffer, filename="image.png")
+    yield from send_image_of_grid(cfg, ctx.message.channel)
 
 @bot.command(pass_context=True)
 @asyncio.coroutine
@@ -611,12 +615,10 @@ def codenames(ctx):
 
     """
     manager = CodenameManager(rows=5, cols=5)
-    cfg = GridConfig(rows=5, cols=5, cells=manager)
-    image = cfg.make_grid()
-    with BytesIO() as buffer:
-        image.save(buffer, 'PNG')
-        buffer.seek(0)
-        yield from bot.send_file(ctx.message.channel, buffer, filename="image.png")
+    cfg  = GridConfig(rows=5, cols=5, cells=manager)
+    cfg1 = GridConfig(rows=5, cols=5, cells=manager.hidden())
+    yield from send_image_of_grid(cfg , ctx.message.channel)
+    yield from send_image_of_grid(cfg1, ctx.message.channel)
 
 try:
     while True:
