65b7c71999a2d1b4a8b905cf440f3471d75f05f9
Silvio Mayolo <mercerenies@comcast.net>
2024-03-09 14:27:58 -0500

    Move dice.py to its own folder

---
+++ dice/__init__.py
@@ -0,0 +1,6 @@
+
+from .parser import dice, parse_dice, AbstractDie
+
+__all__ = (
+    'dice', 'parse_dice', 'AbstractDie',
+)
---
+++ dice/images.py
@@ -0,0 +1,22 @@
+
+"""Images for the different die faces, available for some dice rolls."""
+
+IMAGES_D6 = {
+    1: "res/dice/d6_basic_1.jpg",
+    2: "res/dice/d6_basic_2.jpg",
+    3: "res/dice/d6_basic_3.jpg",
+    4: "res/dice/d6_basic_4.jpg",
+    5: "res/dice/d6_basic_5.jpg",
+    6: "res/dice/d6_basic_6.jpg",
+}
+
+IMAGES_D8 = {
+    1: "res/dice/d8_fireglow_1.jpg",
+    2: "res/dice/d8_fireglow_2.jpg",
+    3: "res/dice/d8_fireglow_3.jpg",
+    4: "res/dice/d8_fireglow_4.jpg",
+    5: "res/dice/d8_fireglow_5.jpg",
+    6: "res/dice/d8_fireglow_6.jpg",
+    7: "res/dice/d8_fireglow_7.jpg",
+    8: "res/dice/d8_fireglow_8.jpg",
+}
